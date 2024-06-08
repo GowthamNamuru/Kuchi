4a4a599c9159318374cfb65a9dc292d103c2f2cf
gowthamnamuru <gowthamnamuri@gmail.com>
2024-06-08 06:33:03 +0530

    Initial Commit

---
+++ Kuchi/KuchiApp.swift
@@ -0,0 +1,17 @@
+//
+//  KuchiApp.swift
+//  Kuchi
+//
+//  Created by Gowtham Namuru on 08/06/24.
+//
+
+import SwiftUI
+
+@main
+struct KuchiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
