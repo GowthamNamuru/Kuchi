30bd9226745ad76d27ce663150564f4790f3c8f9
gowthamnamuru <gowthamnamuri@gmail.com>
2024-06-08 13:36:03 +0530

    Added Quiz flow

--- Kuchi/Welcome/WelcomeView.swift
+++ Kuchi/Welcome/WelcomeView.swift
@@ -8,14 +8,37 @@
 import SwiftUI
 
 struct WelcomeView: View {
+    @EnvironmentObject var userManager: UserManager
+    @ObservedObject var challengesViewModel = ChallengesViewModel()
+    @State var showPractice = false
+
     var body: some View {
-        ZStack {
-            WelcomeBackgroundImage()
-            WelcomeMessageView()
+        if showPractice {
+            PracticeView(challengeTest: $challengesViewModel.currentChallenge, userName: $userManager.profile.name)
+        } else {
+            ZStack {
+                WelcomeBackgroundImage()
+
+                VStack {
+                    Text(verbatim: "Hi, \(userManager.profile.name)")
+                    WelcomeMessageView()
+
+                    Button {
+                        self.showPractice = true
+                    } label: {
+                        HStack {
+                            Image(systemName: "play")
+                            Text(verbatim: "Start")
+                        }
+                    }
+
+                }
+            }
         }
     }
 }
 
 #Preview {
     WelcomeView()
+        .environmentObject(UserManager())
 }
